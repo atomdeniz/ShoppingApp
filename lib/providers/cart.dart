5c4cd4ab0efb305a7c0047bade83e1505b267b2c
denizozogul34 <denizozogul34@outlook.com>
2019-12-30 19:58:17 +0300

    cart items added

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -15,12 +15,23 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
   }
 
+  int get itemCount {
+    return  _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key,cartItem){
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
   void addItem(String productId, double price, String title) {
     if (_items.containsKey(productId)) {
       _items.update(
@@ -43,5 +54,6 @@ class Cart with ChangeNotifier {
         ),
       );
     }
+    notifyListeners();
   }
 }
